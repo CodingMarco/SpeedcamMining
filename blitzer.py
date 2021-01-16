d56689a30c5c2e9e2653af615f985914e9021e9a
CodingMarco <codingmarco@gmail.com>
2021-01-16 21:54:37 +0100

    Asynchronous requests -> now 320x faster!

--- blitzer.py
+++ blitzer.py
@@ -1,61 +1,29 @@
-import sqlite3
-import pandas as pd
-import matplotlib.pyplot as plt
+import asyncio
 from bbox import BBox
 import blitzer_downloader
-import requests
 import time
+import sys
 
 
-def main():
-    sqlquery = "SELECT lat AS latitude,long AS longitude FROM 'blitzermob' WHERE type=1"
-    # https://cdn2.atudo.net/api/3.0/?type=0,1,2,3,4,5,6&z=7&box=48.369631,1.356501,53.838,20.231013
-    url_mask = "https://cdn2.atudo.net/api/3.0/?type=0,1,2,3,4,5,6&z=12&box={},{},{},{}"
-    db = sqlite3.connect("blitzerm.sqlite")
-
+async def main():
     bbox_de = BBox(west=5.98865807458, south=47.3024876979, east=15.0169958839, north=54.983104153, name="Germany")
-    bbox_de.print_graphically()
 
-    df = pd.read_sql_query(sqlquery, db)
-    bbox_db = BBox(west=df.longitude.min(), east=df.longitude.max(),
-                   south=df.latitude.min(), north=df.latitude.max(), name="Database")
-
-    children = bbox_de.get_children_bboxes(8)
-
-    print("Nr. of children: %d" % len(children))
+    tiles = bbox_de.get_children_bboxes(12)
 
     time_start = time.perf_counter()
 
-    for child in children:
-        child_url = url_mask.format(child.south, child.west, child.north, child.east)
-        response = requests.get(child_url, headers=blitzer_downloader.blitzer_app_headers)
-        res_json = response.json()["pois"]
-        #rps = 1/(time.perf_counter() - time_start)
-        #print("{} r/s, url: {}, response: {}".format(rps, child_url, res_json))
-        #time_start = time.perf_counter()
+    speedcams = await blitzer_downloader.download_speedcams(tiles)
 
-    rps = len(children) / (time.perf_counter() - time_start)
-    print("{} r/s".format(rps))
+    elapsed = (time.perf_counter() - time_start)
+    rps = len(tiles) / elapsed
 
+    for speedcam in speedcams:
+        print(speedcam)
 
-    print("Nr. of children: %d" % len(children))
-
-
-
-    exit()
-
-    #os.system("wget https://render.openstreetmap.org/cgi-bin/export?bbox={},{},{},{}&scale=26500000&format=png".format(BBox[0], BBox[2], BBox[1], BBox[3]))
-    ruh_m = plt.imread("map.png")
-
-    fig, ax = plt.subplots(figsize=(8, 7))
-    ax.scatter(df.longitude, df.latitude, zorder=1, alpha=0.9, c='r', s=10)
-    ax.set_title('Mobile und teilstationäre Blitzer')
-    ax.set_xlim(bbox[0], bbox[1])
-    ax.set_ylim(bbox[2], bbox[3])
-    ax.imshow(ruh_m, zorder=0, extent=bbox, aspect='auto')
-    #plt.axis('equal')
-    plt.show()
+    print("Fetched {} tiles / {} speedcams in {} seconds; {} tiles/second;".format(len(tiles), len(speedcams), elapsed, rps), file=sys.stderr)
 
 
 if __name__ == '__main__':
-    main()
+    loop = asyncio.get_event_loop()
+    future = asyncio.ensure_future(main())
+    loop.run_until_complete(future)
--- blitzer_downloader.py
+++ blitzer_downloader.py
@@ -1,9 +1,8 @@
-import aiohttp
 import asyncio
+from aiohttp import ClientSession
+import json
 
-concurrent = 200
-
-blitzer_app_headers = {
+_blitzer_app_headers = {
     "Host": "cdn2.atudo.net",
     "Connection": "keep-alive",
     "Origin": "https://map.atudo.com",
@@ -16,8 +15,37 @@ blitzer_app_headers = {
     "X-Requested-Width": "de.blitzer.plus"
 }
 
+# eg. https://cdn2.atudo.net/api/3.0/?type=0,1,2,3,4,5,6&z=7&box=48.369631,1.356501,53.838,20.231013
+_url_mask = "https://cdn2.atudo.net/api/3.0/?type=0,1,2,3,4,5,6&z=12&box={},{},{},{}"
 
-class BlitzerDownloader:
-    def __init__(self):
-        pass
 
+async def fetch(url: str, session: ClientSession):
+    async with session.get(url, headers=_blitzer_app_headers) as response:
+        return await response.read()
+
+
+def has_speedcams(response: str):
+    return not response.startswith('{"pois":[]')
+
+
+async def download_speedcams(tiles):
+    urls = list()
+    for tile in tiles:
+        urls.append(_url_mask.format(tile.south, tile.west, tile.north, tile.east))
+
+    tasks = []
+
+    async with ClientSession() as session:
+        for url in urls:
+            task = asyncio.ensure_future(fetch(url, session))
+            tasks.append(task)
+
+        responses = await asyncio.gather(*tasks)
+
+    speedcams = list()
+    for response_bytes in responses:
+        response_str: str = response_bytes.decode('unicode_escape')
+        if has_speedcams(response_str):
+            speedcams += json.loads(response_str)['pois']
+
+    return speedcams
